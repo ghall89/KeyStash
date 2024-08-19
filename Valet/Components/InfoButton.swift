bdc3171c322b33d223520eff9bf08e19b8883490
Graham Hall <graham@ghall.dev>
2024-08-19 08:57:03 -0400

    fix 'empty trash' crash

---
+++ Valet/Components/InfoButton.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+struct InfoButton: View {
+	let label: String
+	let value: String
+	let onClick: () -> Void
+	let icon: SFSymbol
+	
+	
+	@State var isHovering = false
+	
+	var body: some View {
+		Button(action: onClick, label: {
+			Image(systemName: getSymbol())
+				.foregroundStyle(.accent)
+				.opacity(isHovering ? 1 : 0.5)
+				.transition(.opacity)
+				.animation(.easeInOut(duration: 0.15), value: isHovering)
+			VStack(alignment: .leading) {
+				Text(label)
+					.font(.caption)
+					.monospaced(false)
+				Text(value)
+			}
+		})
+		.buttonStyle(.plain)
+		.padding(6)
+		.background {
+			RoundedRectangle(cornerSize: CGSize(width: 10, height: 10), style: .circular)
+				.fill(.primary)
+				.opacity(isHovering ? 0.05 : 0)
+				.transition(.opacity)
+				.animation(.easeInOut(duration: 0.15), value: isHovering)
+		}
+		.onHover(perform: { hovering in
+			isHovering = hovering
+		})
+		.multilineTextAlignment(.leading)
+	}
+	
+
+	private func getSymbol() -> String {
+		switch icon {
+			case SFSymbol.document:
+				return "doc.on.doc.fill"
+			case SFSymbol.arrowDown:
+				return "arrow.down.circle.fill"
+		}
+	}
+}
+
+enum SFSymbol {
+	case document
+	case arrowDown
+}
