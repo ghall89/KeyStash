22282ae6fa7f0df7c9ed9c4a6604eccbb96ee484
Graham Hall <graham@ghall.dev>
2026-02-10 08:55:04 -0500

    persist sidebar visibility

--- KeyStash/ViewModels/AppState.swift
+++ KeyStash/ViewModels/AppState.swift
@@ -1,16 +1,24 @@
 import Foundation
 import SwiftUI
 
-// determines various global UI states
+/// determines various global UI states
 final class AppState: ObservableObject {
 	@AppStorage("defaultName") var defaultName: String = ""
 	@AppStorage("defaultEmail") var defaultEmail: String = ""
 	@AppStorage("requireUserAuth") var requireUserAuth: Bool = false
-	//	@AppStorage("requireAuthAfter") var requireAuthAfter: MinutesUntilLocked = .fiveMinutes
-	
+
 	@AppStorage("selectedSort") var selectedSort: SortOptions = .byName
 	@AppStorage("selectedSortOrder") var selectedSortOrder: OrderOptions = .asc
-	
+
+	/// persist sidebar visibility
+	@AppStorage("splitViewVisibility") private var splitViewVisibilityStorage: String = "all"
+
+	@Published var splitViewVisibility: NavigationSplitViewVisibility = .all {
+		didSet {
+			splitViewVisibilityStorage = splitViewVisibility.storageValue
+		}
+	}
+
 	// display modal sheets
 	@Published var showNewAppSheet: Bool = false
 	@Published var showEditAppSheet: Bool = false
@@ -22,8 +30,11 @@ final class AppState: ObservableObject {
 
 	// misc
 	@Published var sidebarSelection: SidebarSelection = .all
-	@Published var selectedLicense = Set<String>()
-	@Published var splitViewVisibility = NavigationSplitViewVisibility.all
+	@Published var selectedLicense: Set<String> = []
+
+	init() {
+		splitViewVisibility = NavigationSplitViewVisibility(storageValue: splitViewVisibilityStorage)
+	}
 
 	func resetSelection() {
 		selectedLicense = []
@@ -34,4 +45,31 @@ enum SidebarSelection: String {
 	case all = "All"
 	case expired = "Expired"
 	case deleted = "Deleted"
+}
+
+private extension NavigationSplitViewVisibility {
+	var storageValue: String {
+		switch self {
+			case .all:
+				"all"
+			case .doubleColumn:
+				"doubleColumn"
+			case .detailOnly:
+				"detailOnly"
+			default:
+				"all"
+		}
+	}
+
+	init(storageValue: String) {
+		self =
+			switch storageValue {
+				case "doubleColumn":
+					.doubleColumn
+				case "detailOnly":
+					.detailOnly
+				default:
+					.all
+			}
+	}
 }
